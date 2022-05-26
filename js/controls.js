66818adb669d2d897e77d7c34a3b7dc7ffe71ad7
Rael Oliveira <raelmoliveira@gmail.com>
2022-05-26 17:16:19 -0300

    First Commit

---
+++ js/controls.js
@@ -0,0 +1,43 @@
+export function Controls({
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  buttonSet
+}) {
+
+  function play() {
+    buttonPlay.classList.add('hide')
+    buttonPause.classList.remove('hide')
+    buttonSet.classList.add('hide')
+    buttonStop.classList.remove('hide')
+  }
+
+  function pause() {
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+  }
+
+  function reset() {
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+    buttonStop.classList.add('hide')
+    buttonSet.classList.remove('hide')
+  }
+
+  function getMinutes() {
+    let newMinutes = prompt('Quantos minutos?')
+    if(!newMinutes) {
+      return false
+    }
+  
+    return newMinutes
+  }
+
+  return {
+    play,
+    pause,
+    reset,
+    getMinutes
+  }
+
+}
---
+++ js/sounds.js
@@ -0,0 +1,24 @@
+export default function() {
+  const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
+  const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
+  const bgAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/bg-audio.mp3?raw=true")
+
+  buttonPressAudio.volume = 0.2
+  kitchenTimer.volume = 0.2
+  bgAudio.volume = 0.3
+  bgAudio.loop = true
+
+  function pressButton() {
+    buttonPressAudio.play()
+  }
+
+  function timeEnd() {
+    kitchenTimer.play()
+  }
+
+  return {
+    pressButton,
+    timeEnd,
+    bgAudio
+  }
+}
